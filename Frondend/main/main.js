a9200f922c2ba587afce272537f4ff01eea2d397
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-04-09 16:39:10 -0500

    se agrega nuevo contenido

---
+++ Frondend/main/main.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", () => {
+    fetch('http://localhost:8080/api/clientes')
+        .then(response => response.json())
+        .then(data => console.log(data));
+})
