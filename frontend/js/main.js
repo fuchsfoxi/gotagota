1d19d512f239834e7c43c1924f246e7fa79c637f
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-04-11 15:34:00 -0500

    cambio de datos en java

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,37 +1,56 @@
+// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
 document.addEventListener("DOMContentLoaded", () => {
-    fetch('http://localhost:8080/api/clientes')
-        .then(response => response.json())
-        .then(data => {
+ fetch("http://localhost:8080/api/clientes")
+  .then((response) => response.json())
+  .then((data) => {
+   //DOM --> <tbody id="table-cliente">
+   const elemento = document.getElementById("table-cliente");
+   for (let i = 0; i < data.length; i++) {
+    //data[i], muestra en forma array
+    let cliente = data[i];
+    //alt + 96
+    let fila = `
+          <tr>
+              <td>${cliente.id}</td>
+              <td>${cliente.nombre}</td>
+              <td>${cliente.apelllido}</td>
+              <td>${cliente.dni}</td>
+              <td>${cliente.telefono}</td>
+              <td>${cliente.direccion}</td> 
+              <td>
+                <!-- Botón Editar (Azul) -->
 
-            const elemento = document.getElementById("table-cliente");
+               <button type="button" class="btn btn-outline-primary">
+                <i class="fa-solid fa-pen-to-square"> </i> 
+               </button>|
 
-            for (let i = 0; i < data.length; i++) {
-                // data[i], muestra  en forma de array
-                let cliente = data[i]
-                //alt + 96 para `, muestra en froma de array
-                let fila = `
-                        <tr>
-                        <td>${cliente.ID}</td>
-                        <td>${cliente.nombre}</td>
-                        <td>${cliente.apellido}</td>
-                        <td>${cliente.DNI}</td>
-                        <td>${cliente.telefono}</td>
-                        <td>${cliente.direccion}</td>
 
-                        <td> 
-                        <button class="btn btn-outline-primary btn-sm me-2">
-                            <i class="fa fa-pencil"></i>
-                        </button>
+               <!-- Botón Eliminar (Rojo) -->
+                <button id="btnEliminar" type="button" class="btn btn-outline-danger">
+                   <i class="fa-solid fa-trash"> </i> 
+                </button>
+              </tr>
 
-                        <button class="btn btn-outline-danger btn-sm">
-                            <i class="fa fa-trash"></i>
-                        </button> 
-                        </td>
-                           `
-                elemento.innerHTML += fila
-                //muestro los resultados en consola  
-                //console.log(cliente)
-                console.log(cliente)
-            }
-        });
+              
+
+              `;
+
+    //Etoquetas
+    elemento.innerHTML += fila;
+    //console.log(Cliente)
+   }
+  });
 });
+
+
+
+// EVENTO DE CLICK EN JAVASC
+// Creamos una variable que almacene el DOM de ese elemento del boton
+
+const btnDelete = document.getElementById("btnEliminar");
+btnDelete.addEventListener("click", function () {
+ alert("Eliminando....");
+
+});
+
+
