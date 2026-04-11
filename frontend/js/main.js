a1dd78fb0ce0a6af02855af70f24d327d6b53305
fuchsfoxi <165415707+fuchsfoxi@users.noreply.github.com>
2026-04-11 16:14:24 -0500

    cambios que implemento el profesor

--- frontend/js/main.js
+++ frontend/js/main.js
@@ -1,139 +1,58 @@
 // EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
-document.addEventListener("DOMContentLoaded", () => {
- fetch("http://localhost:8080/api/clientes")
-  .then((response) => response.json())
-  .then((data) => {
-   //DOM --> <tbody id="table-cliente">
-   const elemento = document.getElementById("table-cliente");
-   for (let i = 0; i < data.length; i++) {
-    //data[i], muestra en forma array
-    let cliente = data[i];
-    //alt + 96
-    let fila = `
-          <tr>
-              <td>${cliente.id}</td>
-              <td>${cliente.nombre}</td>
-              <td>${cliente.apelllido}</td>
-              <td>${cliente.dni}</td>
-              <td>${cliente.telefono}</td>
-              <td>${cliente.direccion}</td> 
-              <td>
-                <!-- Botón Editar (Azul) -->
-
-               <button type="button" class="btn btn-outline-primary">
-                <i class="fa-solid fa-pen-to-square"> </i> 
-               </button>|
-
-
-               <!-- Botón Eliminar (Rojo) -->
-                <button id="btnEliminar" data-idcliente = ${cliente.id  } type="button" class="btn btn-outline-danger">
-                   <i class="fa-solid fa-trash"> </i> 
-                </button>
-              </tr>
-
-              
-
-              `;
-
-    //Etoquetas
-    elemento.innerHTML += fila;
-    //console.log(Cliente)
-   }
-  });
-});
-
-
-
-// EVENTOS EN JAVASCRIPT (CLICK, CARGAR, KEY,)
 
 document.addEventListener("DOMContentLoaded", () => {
 
- fetch("http://localhost:8080/api/clientes")
-
-  .then((response) => response.json())
-
-  .then((data) => {
-
-   //DOM --> <tbody id="table-cliente">
-
-   const elemento = document.getElementById("table-cliente");
-
-   for (let i = 0; i < data.length; i++) {
-
-    //data[i], muestra en forma array
-
-    let cliente = data[i];
-
-    //alt + 96
-
-    let fila = `
-
-          <tr>
-
-              <td>${cliente.id}</td>
-
-              <td>${cliente.nombre}</td>
-
-              <td>${cliente.apelllido}</td>
-
-              <td>${cliente.dni}</td>
-
-              <td>${cliente.telefono}</td>
-
-              <td>${cliente.direccion}</td> 
-
-              <td>
-
-                <!-- Botón Editar (Azul) -->
-
-               <button type="button" class="btn btn-outline-primary">
-
-                <i class="fa-solid fa-pen-to-square"></i> Editar
-
-               </button>|
-
-
-
-               <!-- Botón Eliminar (Rojo) -->
-
-                <button id="btnEliminar" data-idcliente ${cliente.id} type="button" class="btn btn-outline-danger">
-
-                   <i class="fa-solid fa-trash"></i> Eliminar
-
-                </button>
-
-              </tr>
-
-              
-
-              `;
-
-    //Etoquetas
-
-    elemento.innerHTML += fila;
-
-    //console.log(Cliente)
-
-   }
-
-  });
+  fetch("http://localhost:8080/api/clientes")
+    .then((response) => response.json())
+    .then((data) => {
+      // DOM -> <tbody id="table-cliente">
+      const elemento = document.getElementById("table-cliente");
+      for (let i = 0; i < data.length; i++) {
+        //data[i], muestra en forma de array
+        let cliente = data[i];
+        // alt + 96
+        let fila = `
+                            <tr>
+                            <td>${cliente.id}</td>
+                            <td>${cliente.nombre}</td>
+                            <td>${cliente.apellido}</td>
+                            <td>${cliente.dni}</td>
+                            <td>${cliente.telefono}</td>
+                            <td>${cliente.direccion}</td>
+                            <td> 
+                                <button class="btn btn-outline-primary me-2">
+                                    <i class="fas fa-edit"></i> Editar
+                                </button>
+                                <button id="btnEliminar" data-idcliente = ${cliente.id} class="btn btn-outline-danger">
+                                    <i class="fas fa-trash"></i> Eliminar
+                                </button>
+                            </td>
+                            </tr>                
+                           `;
+        elemento.innerHTML += fila;
+      }
+    });
 
 });
 
-
-
-// EVENTO DE CLICK EN JAVASC
-
-// Creamos una variable que almacene el DOM de ese elemento del boton
-
+// EVENTO DE CLICK EN JAVASCRIPT
+//Creamos una variable que almacene el DOM de ese elemento del boton
 document.addEventListener("click", function (e) {
- const btnDelete = e.target.closest("#btnEliminar");
- if (btnDelete) {
-  alert("Elminando....");
-  const id = btnDelete.dataset.idcliente;
-  fetch(`http://localhost:8080/api/clientes/${id}`, 
-    {
-        method: 'DELETE'
+  const btnDelete = e.target.closest("#btnEliminar");
+    if (btnDelete) { //TRUE o 1
+        alert("Eliminando...");
+        const id = btnDelete.dataset.idcliente;
+        //console.log(id) para en consola que ID es nada mas
+       // fetch(`http://localhost:8080/api/clientes/${id}`, {
+        fetch("http://localhost:8080/api/clientes/"+id, {
+          method: 'DELETE'
+        })
+        .then(response => {
+            if (response.ok) {
+              alert('Cliente eliminado correctamente');
+              location.reload(); // Recargar la página para reflejar los cambios
+            } else {
+              alert('Error al eliminar el cliente');
+        }})
     }
-  )
- }})
+});
